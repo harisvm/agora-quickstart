160b6463bdfe0794cf162a5e527e9438b57ca08c
harrys <harry>
2024-09-07 21:13:13 +0530

    added prod config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./agoraLogic.js",
-    output: {
-        filename: "bundledAgoraLogic.js",
-        path: path.resolve(__dirname, "./dist"),
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, './'), 
-        },
-        compress: true,
-        port: 9000,
-    },
+  entry: './agoraLogic.js',
+  output: {
+    filename: 'bundledAgoraLogic.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production', // Ensure this is set to 'production'
+  module: {
+    rules: [
+      // Add any loaders if needed for processing other file types
+    ],
+  },
+  optimization: {
+    minimize: true, // Ensure minification for production
+  },
+  // Exclude devServer configuration as it's not used in production builds
 };
